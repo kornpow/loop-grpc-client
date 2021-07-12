75718098ce2c06280f61b5fd35d4db054e7634b1
Sam Korn <korn94sam@gmail.com>
2021-07-11 21:34:29 -0600

    add an example usage script

--- examples/example.py
+++ examples/example.py
@@ -10,8 +10,6 @@ from datetime import datetime, timedelta
 from loopgrpc import LoopClient
 from protobuf_to_dict import protobuf_to_dict
 
-import asyncio
-
 credential_path = Path("/home/skorn/Documents/creds/kungmeow/loop")
 
 mac = str(credential_path.joinpath("loop.macaroon").absolute())
@@ -22,6 +20,27 @@ loop = LoopClient(
 	macaroon_filepath=mac,
 	cert_filepath=tls
 )
+
+
+
+
+from pathlib import Path
+import json
+from pprint import pprint
+import os
+import base64
+from time import sleep
+from datetime import datetime, timedelta
+
+# Pip installed Modules
+from loopgrpc import LoopClient
+from protobuf_to_dict import protobuf_to_dict
+
+credential_path = Path("/home/skorn/.loop/mainnet")
+
+mac = str(credential_path.joinpath("loop.macaroon").absolute())
+tls = str(credential_path.joinpath("tls.cert").absolute())
+
 
 loop = LoopClient(
 	"127.0.0.1:11010",
